b1b0c9599baa6e1ceecefc6f9b522031bb8e01b3
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-09-10 11:49:50 +0330

    Add news rss view

---
+++ core/core/views.py
@@ -0,0 +1,12 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from . import parser
+from .ut_news import UtNews
+
+
+@api_view(('GET',))
+def news_rss(request):
+    return Response({
+        'news': parser.parse_news().to_dict(),
+    })
