df0909fe92bf8e0de7c1e7cbfd64a3fc7454b05e
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-09-10 11:38:46 +0330

    Create ut_news.py

---
+++ core/core/ut_news.py
@@ -0,0 +1,11 @@
+class UtNews:
+    def __init__(self, img_src, img_alt, short_title, title, news_detail_url, is_new, brief, category, date):
+        self.img_src = img_src
+        self.img_alt = img_alt
+        self.short_title = short_title
+        self.title = title
+        self.news_detail_url = news_detail_url
+        self.is_new = is_new
+        self.brief = brief
+        self.category = category
+        self.date = date
