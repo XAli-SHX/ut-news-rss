e0416ff17ba89c58f710af30449746d8e0364f93
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-09-10 12:20:23 +0330

    Add JSON renderer to only show news in JSON format

--- core/core/views.py
+++ core/core/views.py
@@ -1,11 +1,12 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, renderer_classes
+from rest_framework.renderers import JSONRenderer
 from rest_framework.response import Response
 
 from . import parser
-from .ut_news import UtNews
 
 
 @api_view(('GET',))
+@renderer_classes([JSONRenderer])
 def news_rss(request):
     return Response({
         'news': parser.parse_news(),
