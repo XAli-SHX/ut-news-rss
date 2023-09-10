7cc8e9ba7320d24fde9d4ad755ba87abe017e972
Ali Shayanpoor <56270506+XAli-SHX@users.noreply.github.com>
2023-09-10 11:42:01 +0330

    Add to_dict method

--- core/core/ut_news.py
+++ core/core/ut_news.py
@@ -9,3 +9,16 @@ class UtNews:
         self.brief = brief
         self.category = category
         self.date = date
+
+    def to_dict(self):
+        return {
+            'img_src': self.img_src,
+            'img_alt': self.img_alt,
+            'short_title': self.short_title,
+            'title': self.title,
+            'news_detail_url': self.news_detail_url,
+            'is_new': self.is_new,
+            'brief': self.brief,
+            'category': self.category,
+            'date': self.date
+        }
